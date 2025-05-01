dfb1b9bdf7ff2d17c287718e057675078847244b
bakerinho <dbaker2137@gmail.com>
2025-05-02 01:00:13 +0200

    a

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-const toggle = document.querySelector('.theme-toggle')
+// toggle dark/light mode
 
-toggle.addEventListener('click', () => {
+const themeToggle = document.querySelector('.theme-toggle')
+
+themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('light');
     if (document.body.classList.contains('light')) {
       localStorage.setItem('theme', 'light');
     } else {
       localStorage.setItem('theme', 'dark');
     }
+})
+
+// cart toggle
+
+const cartToggle = document.querySelector('.cart-toggle')
+
+cartToggle.addEventListener('click', () => {
+  
 })
