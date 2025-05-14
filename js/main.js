d1dcd6d44903fd6e0c62caa6290d1fbd3264326c
bakerihno <dbaker2137@gmail.com>
2025-05-15 00:01:18 +0200

    added cart functionality

--- js/main.js
+++ js/main.js
@@ -37,27 +37,43 @@ const contentContainer = document.querySelector('.content-container')
 
 menuToggle.addEventListener('click', () => {
   const isOpen = menu.getAttribute("aria-expanded") == 'true'
-  setMenuState(menu, !isOpen)
+  setMenuState(!isOpen)
   console.log(isOpen)
 })
 
-document.addEventListener('click', (e) => {
-  const isClickInside = menu.contains(e.target) || menuToggle.contains(e.target);
-  if (!isClickInside) {
-    setMenuState(menu, false);
-  }
-})
 
-function setMenuState(div, isOpen){
-  div.setAttribute("aria-expanded", String(isOpen))
+function setMenuState(isOpen){
+  menu.setAttribute("aria-expanded", String(isOpen))
   contentContainer.style.filter = isOpen ? 'blur(10px)' : 'none';
   contentContainer.style.transition = 'filter 0.25s ease';
 }
 
 // cart toggle
 const cartToggle = document.querySelector('.cart-toggle')
+const cartExit = document.querySelector('.cart-exit')
+const cart = document.querySelector('.cart')
+const container = document.querySelector('.container')
 
 cartToggle.addEventListener('click', () => {
-
+  const isOpen = cart.getAttribute("aria-expanded") == 'true'
+  setCartState(!isOpen)
 })
 
+cartExit.addEventListener('click', () => {
+  setCartState(false)
+})
+
+function setCartState(isOpen){
+  cart.setAttribute("aria-expanded", String(isOpen))
+  container.style.filter = isOpen ? 'brightness(70%)' : 'none';
+  container.style.transition = 'filter 0.3s ease-out';
+}
+
+// closes menu/cart if clicked somewhere else
+document.addEventListener('click', (e) => {
+  const isClickInside = (menu.contains(e.target) || menuToggle.contains(e.target) ||  cart.contains(e.target) || cartToggle.contains(e.target));
+  if (!isClickInside) {
+    setMenuState(false)
+    setCartState(false)
+  }
+})
