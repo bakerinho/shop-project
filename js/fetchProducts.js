c5a9983ec3f882a1ee832cffb8d5b44ad18cdfbd
bakerinho <dbaker2137@gmail.com>
2025-05-19 12:46:00 +0000

    general improvements

--- js/fetchProducts.js
+++ js/fetchProducts.js
@@ -1,4 +1,5 @@
 // json-server --watch data/products.json --port 8888
+// npx json-server --watch data/products.json --port 8888
 
 export function fetchProducts() {
   const productsContainer = document.querySelector('.items-container')
